28a3a28a4f0037beb2c954237c0d022c5788b250
Gabriel Braga <gbraga.dev@gmail.com>
2019-02-19 18:03:08 -0300

    adicionado teste para Curso

---
+++ CursoTDD.Test/Curso/CursoTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CursoTDD.Test.Curso
+{
+    public class CursoTest 
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+            var a = 1;
+            var b = 1;
+
+            Assert.Equal(a, b);
+        }
+
+
+        [Fact]
+        public void DeveTerNome()
+        {
+            Assert.Equal(1, 2);
+        }
+    }
+}
